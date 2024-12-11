8da666756f2e6d5d74cae1e44e7e31b2584a3c12
K S Saivaibhav <saivaibhavks2022@gmail.com>
2024-12-11 19:17:55 +0530

    added logic

--- src/Tabs.jsx
+++ src/Tabs.jsx
@@ -6,12 +6,44 @@ const Tabs = () => {
 
   const [activeTab, setActiveTab] = useState("profile");
 
+  const changeSection = (section) => {
+    setActiveTab(section);
+  };
+
   return (
     <div className="container">
       <div className="tabs-action">
-        <button>Profile</button>
-        <button>Interests</button>
-        <button>Settings</button>
+        <button
+          style={{
+            backgroundColor: activeTab === "profile" ? "red" : null,
+            cursor: "pointer",
+          }}
+          onClick={() => changeSection("profile")}
+        >
+          Profile
+        </button>
+        <button
+          style={{
+            backgroundColor: activeTab === "interests" ? "red" : null,
+            cursor: "pointer",
+          }}
+          onClick={() => changeSection("interests")}
+        >
+          Interests
+        </button>
+        <button
+          style={{
+            backgroundColor: activeTab === "settings" ? "red" : null,
+            cursor: "pointer",
+          }}
+          onClick={() => changeSection("settings")}
+        >
+          Settings
+        </button>
+      </div>
+
+      <div className="body">
+        <p>{activeTab}</p>
       </div>
     </div>
   );
