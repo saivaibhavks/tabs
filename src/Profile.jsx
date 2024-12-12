2717c71f744f9e6e9ff48511f346ed908e22ae7e
K S Saivaibhav <saivaibhavks2022@gmail.com>
2024-12-12 10:13:29 +0530

    added form

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -6,15 +6,15 @@ const Profile = () => {
       <div>Profile Details</div>
       <div className="full-name">
         <label htmlFor="fullName">FullName</label>
-        <input type="text" placeholder="Enter Full Name" />
+        <input type="text" placeholder="Enter Full Name" required />
       </div>
       <div className="age">
         <label htmlFor="age">Age</label>
-        <input type="number" placeholder="Enter Age" />
+        <input type="number" placeholder="Enter Age" required />
       </div>
       <div className="email">
         <label htmlFor="email">Email</label>
-        <input type="text" placeholder="Enter Email" />
+        <input type="text" placeholder="Enter Email" required />
       </div>
     </div>
   );
